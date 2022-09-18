09d6b4659047ab3e7e852e20bcd0131839cb5961
dhirush <88309520+dhirush@users.noreply.github.com>
2022-09-18 23:52:12 +0530

    Add files via upload

---
+++ models/todo.js
@@ -0,0 +1,98 @@
+"use strict";
+const { Model, Op } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    
+    static async addTask(params) {
+      return await Todo.create(params);
+    }
+    static async showList() {
+      console.log("My Todo list \n");
+
+      console.log("Overdue");
+
+      const overdueItems = await Todo.overdue();
+      console.log(
+        overdueItems.map((item) => item.displayableString()).join("\n")
+      );
+      console.log("\n");
+
+      console.log("Due Today");
+
+      const dueItems = await Todo.dueToday();
+      console.log(dueItems.map((item) => item.displayableString()).join("\n"));
+      console.log("\n");
+
+      console.log("Due Later");
+      const dueLaterItems = await Todo.dueLater();
+      console.log(
+        dueLaterItems.map((item) => item.displayableString()).join("\n")
+      );
+    }
+
+    static async overdue() {
+      return Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: new Date(),
+          },
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async dueToday() {
+      return Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date(),
+          },
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async dueLater() {
+      return Todo.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date(),
+          },
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+
+    static async markAsComplete(id) {
+      return Todo.update(
+        { completed: true },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+    }
+
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      let date =
+        this.dueDate === new Date().toLocaleDateString("en-CA")
+          ? ""
+          : this.dueDate;
+      return `${this.id}. ${checkbox} ${this.title} ${date}`.trim();
+    }
+  }
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
+  return Todo;
+};
